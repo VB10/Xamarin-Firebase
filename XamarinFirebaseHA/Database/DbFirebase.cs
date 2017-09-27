06ee28901ff15e0dc3575c58b484c5d81cff0cc7
Veli Bacık <tercih2017bsm@gmail.com>
2017-09-27 19:34:06 +0300

    Firebase Database post 1
    Student listesi oluşturuyor ve ekranda gösteriyoruz

---
+++ XamarinFirebaseHA/Database/DbFirebase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Firebase.Xamarin.Database;
+
+namespace XamarinFirebaseHA
+{
+	public class DbFirebase
+	{
+		FirebaseClient client;
+		public DbFirebase()
+		{
+			client = new FirebaseClient("buraya kendi url girin");
+		}
+
+		public async Task<List<Student>> getList()
+		{
+			var list = (await client
+				.Child("Student")
+				.OnceAsync<Student>())
+				.Select(item =>
+						new Student
+						{
+							age=item.Object.age,
+							name=item.Object.name
+			}).ToList();
+
+
+			return list;
+
+		}
+
+	}
+}
